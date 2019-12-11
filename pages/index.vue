6a0e817650bbc8d7192f6341f6c60e78ad009bd2
Tyus Durant <contact@tyusdurant.com>
2019-12-11 11:36:30 -0500

    endofday1211

---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <main>
+    <div class="background">
+      <app-masthead/>
+    </div>
+    <div class="headings">
+      <h1>Tacos
+    <br>I love tocos
+    </h1>
+    </div>
+    <div class="links"><p>Go follow me</p></div>
+    <div class="nextpage">Projects</div>
+    
+    
+  </main>
+</template>
+
+<script>
+import AppMasthead from "@/components/AppMasthead.vue";
+
+export default {
+  components: {
+    AppMasthead
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+main {
+  width: 100vw;
+  height: calc(100vh - 100px);
+  display: grid;
+  grid-template-columns: repeat(2, 100px) repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr) 50px 100px;
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+}
+
+.background {
+  grid-area: 1 / 3 / 5 / 6;
+}
+.headings {
+  grid-area: 2 / 2 / 3 / 6;
+}
+.links {
+  padding: 30px;
+  grid-area: 5 / 1 / 6 / 5;
+}
+.nextpage {
+  background: #eee;
+  color: black;
+  padding: 30px;
+  grid-area: 4 / 5 / 6 / 6;
+}
+</style>
---
+++ pages/projects.vue
@@ -0,0 +1,58 @@
+<template>
+    <main>
+        <section>
+         <h1>Projects page</h1>
+         <div v-for="project in githubProjects" :key="project.id">
+             <h3><a :href="project.html_url">{{project.name}}</a></h3>
+             <p>{{project.description}}</p>
+        </div>   
+        </section>
+        <aside>
+            <h3>About</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea unde aut laudantium, dolorum dolores odit voluptatibus reprehenderit fugit iste quam consequatur nostrum praesentium consectetur libero deleniti at vel quisquam quaerat?</p>
+        </aside>
+    </main>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["githubProjects"])
+  }
+  //moved to plugin
+  //   mounted() {
+  //     this.$store.dispatch("getGithubProjects");
+  //   }
+};
+</script>
+
+<style lang="scss" scoped>
+main {
+  width: 80vw;
+  margin: 2% 8%;
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-template-rows: 1fr;
+  grid-column-gap: 50px;
+  grid-row-gap: 0px;
+}
+
+section {
+  background: white;
+  color: black;
+  padding: 30px;
+  grid-area: 1 / 1 / 2 / 2;
+  a {
+    color: black;
+    cursor: pointer;
+  }
+  div {
+    margin: 10px 0;
+  }
+}
+aside {
+  grid-area: 1 / 2 / 2 / 3;
+}
+</style>
